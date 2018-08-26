ec1b3b1e9306ef41f588ecfc0cf4f00c89fb2e82
Evan Huang <evan.huang@oocl.com>
2018-08-26 12:48:38 +0800

    DesignPattern-<creational>-<simplefactory> - 使用反射解决开闭原则问题

--- src/main/java/com/jluzh/evan/design/pattern/creational/simplefactory/Test.java
+++ src/main/java/com/jluzh/evan/design/pattern/creational/simplefactory/Test.java
@@ -1,12 +1,18 @@
 package com.jluzh.evan.design.pattern.creational.simplefactory;
 
 public class Test {
+//    public static void main(String[] args) {
+//        VideoFactory videoFactory = new VideoFactory();
+//        Video video = videoFactory.getVideo("python");
+//        if (video == null) {
+//            return;
+//        }
+//        video.produce();
+//    }
+    // 使用反射解决开闭原则问题
     public static void main(String[] args) {
         VideoFactory videoFactory = new VideoFactory();
-        Video video = videoFactory.getVideo("python");
-        if (video == null) {
-            return;
-        }
+        Video video = videoFactory.getVideo(PythonVideo.class);
         video.produce();
     }
 
--- src/main/java/com/jluzh/evan/design/pattern/creational/simplefactory/VideoFactory.java
+++ src/main/java/com/jluzh/evan/design/pattern/creational/simplefactory/VideoFactory.java
@@ -2,14 +2,27 @@ package com.jluzh.evan.design.pattern.creational.simplefactory;
 
 public class VideoFactory {
 
+//   public Video getVideo(String type){
+//        if ("java".equals(type)) {
+//            return new JavaVideo();
+//        } else if ("python".equals(type)) {
+//            return new PythonVideo();
+//        } else {
+//            return null;
+//        }
+//   }
 
-   public Video getVideo(String type){
-        if ("java".equals(type)) {
-            return new JavaVideo();
-        } else if ("python".equals(type)) {
-            return new PythonVideo();
-        } else {
-            return null;
+    public Video getVideo(Class clazz) {
+        Video video = null;
+        try {
+            video = (Video) Class.forName(clazz.getName()).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-   }
+        return video;
+    }
 }
