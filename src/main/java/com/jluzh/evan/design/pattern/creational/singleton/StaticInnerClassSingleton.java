de9852e83fe56c0733277de0ad45cedc12019613
desertTown <13631260372@163.com>
2018-08-27 00:21:01 +0800

    DesignPattern-<创建型>-<懒汉式单例>- 双重锁-延迟加载-使用静态内部类的形式

---
+++ src/main/java/com/jluzh/evan/design/pattern/creational/singleton/StaticInnerClassSingleton.java
@@ -0,0 +1,14 @@
+package com.jluzh.evan.design.pattern.creational.singleton;
+
+public class StaticInnerClassSingleton {
+    private static class InnerClass{
+        private static StaticInnerClassSingleton staticInnerClassSingleton = new StaticInnerClassSingleton();
+    }
+
+    public static StaticInnerClassSingleton getInstance() {
+        return InnerClass.staticInnerClassSingleton;
+    }
+
+    private StaticInnerClassSingleton() {
+    }
+}
--- src/main/java/com/jluzh/evan/design/pattern/creational/singleton/T.java
+++ src/main/java/com/jluzh/evan/design/pattern/creational/singleton/T.java
@@ -4,7 +4,8 @@ public class T implements Runnable{
     @Override
     public void run() {
 //        LazySingleton lazySingleton = LazySingleton.getInstance();
-        LazyDoubleCheckSingleton instance = LazyDoubleCheckSingleton.getInstance();
+//        LazyDoubleCheckSingleton instance = LazyDoubleCheckSingleton.getInstance();
+        StaticInnerClassSingleton instance = StaticInnerClassSingleton.getInstance();
         System.out.println(Thread.currentThread().getName()+":"+ instance);
     }
 }
