b6f8729899128d28dde8a361c2fd7d7134046461
Evan Huang <evan.huang@oocl.com>
2018-08-26 11:03:38 +0800

    里氏替换原则  尽量不重写父类方法

--- src/main/java/com/jluzh/evan/design/principle/openclose/Test.java
+++ src/main/java/com/jluzh/evan/design/principle/openclose/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         ICourse iCourse = new JavaDiscountCourse(25, "java", 99.25d);
         JavaDiscountCourse javaDiscountCourse = (JavaDiscountCourse) iCourse;
-        System.out.println("id: "+ javaDiscountCourse.getId() + " 课程名称：" + javaDiscountCourse.getName() + " 课程打折后的价格："+ javaDiscountCourse.getPrice()+ " 课程原价：" + javaDiscountCourse.getOriginPrice());
+        System.out.println("id: "+ javaDiscountCourse.getId() + " 课程名称：" + javaDiscountCourse.getName() + " 课程打折后的价格："+ javaDiscountCourse.getDiscountPrice()+ " 课程原价：" + javaDiscountCourse.getPrice());
     }
 }
