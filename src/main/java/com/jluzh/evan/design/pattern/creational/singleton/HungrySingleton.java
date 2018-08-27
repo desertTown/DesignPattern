7ca1f5730500857d20267eb8655df25545fd5a48
Evan Huang <evan.huang@oocl.com>
2018-08-27 20:11:23 +0800

    DesignPattern-<创建型>-<饿汉式单例>- 反射攻击, 生成2个不同的实例.  对于恶汉式的话, 通过在构造器中判空可以避免, 但是懒汉式就避免不了了, 懒汉式即便通过flag在构造器中去判断, 反射也能通过反射来改变字段的值

--- src/main/java/com/jluzh/evan/design/pattern/creational/singleton/HungrySingleton.java
+++ src/main/java/com/jluzh/evan/design/pattern/creational/singleton/HungrySingleton.java
@@ -5,7 +5,11 @@ import java.io.Serializable;
 public class HungrySingleton implements Serializable{
     private final static HungrySingleton hungrySingleton = new HungrySingleton();
 
-    private HungrySingleton(){}
+    private HungrySingleton(){
+        if (hungrySingleton != null) {
+            throw new RuntimeException("单例构造器禁止反射调用！");
+        }
+    }
 
     public static HungrySingleton getInstance() {
         return hungrySingleton;
--- src/main/java/com/jluzh/evan/design/pattern/creational/singleton/Test.java
+++ src/main/java/com/jluzh/evan/design/pattern/creational/singleton/Test.java
@@ -1,9 +1,11 @@
 package com.jluzh.evan.design.pattern.creational.singleton;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class Test {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 //        LazySingleton lazySingleton = LazySingleton.getInstance();
 //        Thread t1 = new Thread(new T());
 //        Thread t2 = new Thread(new T());
@@ -11,24 +13,42 @@ public class Test {
 //        t2.start();
 //        System.out.println("program end");
 
-        HungrySingleton instance = HungrySingleton.getInstance();
-        ObjectOutputStream oos = null;
-        ObjectInputStream ois = null;
-        try {
-            oos = new ObjectOutputStream(new FileOutputStream("single_file"));
-            oos.writeObject(instance);
-            ois = new ObjectInputStream(new FileInputStream("single_file"));
+        // 序列化反序列化导致单例破坏
+//        HungrySingleton instance = HungrySingleton.getInstance();
+//        ObjectOutputStream oos = null;
+//        ObjectInputStream ois = null;
+//        try {
+//            oos = new ObjectOutputStream(new FileOutputStream("single_file"));
+//            oos.writeObject(instance);
+//            ois = new ObjectInputStream(new FileInputStream("single_file"));
+//
+//            HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+//
+//            System.out.println(instance);
+//            System.out.println(newInstance);
+//            System.out.println(instance == newInstance);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            ois.close();
+//            oos.close();
+//        }
+        // 反射调用
+//        Class objectClass = HungrySingleton.class;
+        Class objectClass = LazySingleton.class;
 
-            HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+        Constructor constructor = objectClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+//        HungrySingleton instance = HungrySingleton.getInstance();
+//        HungrySingleton newInstance = (HungrySingleton) constructor.newInstance();
+        LazySingleton instance = LazySingleton.getInstance();
+        LazySingleton newInstance = (LazySingleton) constructor.newInstance();
 
-            System.out.println(instance);
-            System.out.println(newInstance);
-            System.out.println(instance == newInstance);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            ois.close();
-            oos.close();
-        }
+        System.out.println(instance);
+        System.out.println(newInstance);
+        System.out.println(instance == newInstance);
+
+
+
     }
 }
