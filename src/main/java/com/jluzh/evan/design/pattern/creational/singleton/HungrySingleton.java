1927f45bf826960399fab61f29b11662122c94bc
Evan Huang <evan.huang@oocl.com>
2018-08-27 19:36:20 +0800

    DesignPattern-<创建型>-<饿汉式单例>- 序列化和反序列化会破坏单例模式， 2者不是同一个对象， 但是介意通过单例类中添加这个方法readResolve， name序列反序列之后就还是同一个对象了， 具体原理看资料

--- src/main/java/com/jluzh/evan/design/pattern/creational/singleton/HungrySingleton.java
+++ src/main/java/com/jluzh/evan/design/pattern/creational/singleton/HungrySingleton.java
@@ -1,11 +1,17 @@
 package com.jluzh.evan.design.pattern.creational.singleton;
 
-public class HungrySingleton {
+import java.io.Serializable;
+
+public class HungrySingleton implements Serializable{
     private final static HungrySingleton hungrySingleton = new HungrySingleton();
 
     private HungrySingleton(){}
 
     public static HungrySingleton getInstance() {
+        return hungrySingleton;
+    }
+
+    private Object readResolve() {
         return hungrySingleton;
     }
 }
--- src/main/java/com/jluzh/evan/design/pattern/creational/singleton/Test.java
+++ src/main/java/com/jluzh/evan/design/pattern/creational/singleton/Test.java
@@ -1,12 +1,34 @@
 package com.jluzh.evan.design.pattern.creational.singleton;
 
+import java.io.*;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 //        LazySingleton lazySingleton = LazySingleton.getInstance();
-        Thread t1 = new Thread(new T());
-        Thread t2 = new Thread(new T());
-        t1.start();
-        t2.start();
-        System.out.println("program end");
+//        Thread t1 = new Thread(new T());
+//        Thread t2 = new Thread(new T());
+//        t1.start();
+//        t2.start();
+//        System.out.println("program end");
+
+        HungrySingleton instance = HungrySingleton.getInstance();
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream("single_file"));
+            oos.writeObject(instance);
+            ois = new ObjectInputStream(new FileInputStream("single_file"));
+
+            HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+
+            System.out.println(instance);
+            System.out.println(newInstance);
+            System.out.println(instance == newInstance);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            ois.close();
+            oos.close();
+        }
     }
 }
