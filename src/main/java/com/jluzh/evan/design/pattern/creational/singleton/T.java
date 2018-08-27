4efcf06cda5fa17dba822d82b90e44b492125ad2
desertTown <13631260372@163.com>
2018-08-28 00:18:41 +0800

    DesignPattern-<创建型>-<单例>-基于ThreadLocal 的‘单例’，这里的单例仅仅局限于同个线程的单例

--- src/main/java/com/jluzh/evan/design/pattern/creational/singleton/T.java
+++ src/main/java/com/jluzh/evan/design/pattern/creational/singleton/T.java
@@ -7,8 +7,10 @@ public class T implements Runnable{
 //        LazyDoubleCheckSingleton instance = LazyDoubleCheckSingleton.getInstance();
 //        StaticInnerClassSingleton instance = StaticInnerClassSingleton.getInstance();
 
-        ContainerSingleton.putInstance("object", new Object());
-        Object instance = ContainerSingleton.getInstance("object");
+//        ContainerSingleton.putInstance("object", new Object());
+//        Object instance = ContainerSingleton.getInstance("object");
+
+        ThreadLocalInstance instance = ThreadLocalInstance.getInstance();
         System.out.println(Thread.currentThread().getName()+":"+ instance);
     }
 }
