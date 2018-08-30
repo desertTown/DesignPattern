0bff215cfc07a4648cdb1ec82c34a9387b7c9628
desertTown <13631260372@163.com>
2018-08-31 00:13:43 +0800

    DesignPattern-<结构型>-<桥接模式>

---
+++ src/main/java/com/jluzh/evan/design/pattern/structural/bridge/Account.java
@@ -0,0 +1,6 @@
+package com.jluzh.evan.design.pattern.structural.bridge;
+
+public interface Account {
+    Account openAccount();
+    void showAccountType();
+}
---
+++ src/main/java/com/jluzh/evan/design/pattern/structural/bridge/Bank.java
@@ -0,0 +1,11 @@
+package com.jluzh.evan.design.pattern.structural.bridge;
+
+public abstract class Bank {
+    protected Account account;
+
+    public Bank(Account account) {
+        this.account = account;
+    }
+
+    abstract Account openAccount();
+}
