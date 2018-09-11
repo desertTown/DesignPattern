3c8bbb47238e9fc9551f10d4a7371972f5728282
Evan Huang <evan.huang@oocl.com>
2018-09-11 20:03:59 +0800

    DesignPattern-<结构型>-<静态代理>

---
+++ src/main/java/com/jluzh/evan/design/pattern/structural/proxy/IOrderDao.java
@@ -0,0 +1,7 @@
+package com.jluzh.evan.design.pattern.structural.proxy;
+
+
+public interface IOrderDao {
+    int insert(Order order);
+
+}
---
+++ src/main/java/com/jluzh/evan/design/pattern/structural/proxy/IOrderService.java
@@ -0,0 +1,6 @@
+package com.jluzh.evan.design.pattern.structural.proxy;
+
+
+public interface IOrderService {
+    int saveOrder(Order order);
+}
