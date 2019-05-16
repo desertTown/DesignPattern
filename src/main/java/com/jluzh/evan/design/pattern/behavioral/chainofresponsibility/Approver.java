2e647ed78bce983af73ca0cdecf16fc9167a9124
desertTown <13631260372@163.com>
2019-05-16 23:10:39 +0800

    DesignPattern-<行为型>-<责任链模式>

---
+++ src/main/java/com/jluzh/evan/design/pattern/behavioral/chainofresponsibility/Approver.java
@@ -0,0 +1,11 @@
+package com.jluzh.evan.design.pattern.behavioral.chainofresponsibility;
+
+
+public abstract class Approver {
+    protected Approver approver;
+
+    public void setNextApprover(Approver approver){
+        this.approver = approver;
+    }
+    public abstract void deploy(Course course);
+}
---
+++ src/main/java/com/jluzh/evan/design/pattern/behavioral/chainofresponsibility/Test.java
@@ -0,0 +1,18 @@
+package com.jluzh.evan.design.pattern.behavioral.chainofresponsibility;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Approver articleApprover = new ArticleApprover();
+        Approver videoApprover = new VideoApprover();
+
+        Course course = new Course();
+        course.setName("Java设计模式精讲 -- By Evan");
+        course.setArticle("Java设计模式精讲的手记");
+        course.setVideo("Java设计模式精讲的视频");
+
+        articleApprover.setNextApprover(videoApprover);
+
+        articleApprover.deploy(course);
+    }
+}
