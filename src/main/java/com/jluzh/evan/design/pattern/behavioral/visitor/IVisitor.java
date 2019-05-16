beefd810e1540e617ab8021b85c45cbe5b36fe03
desertTown <13631260372@163.com>
2019-05-16 23:32:25 +0800

    DesignPattern-<行为型>-<访问者模式>

---
+++ src/main/java/com/jluzh/evan/design/pattern/behavioral/visitor/IVisitor.java
@@ -0,0 +1,11 @@
+package com.jluzh.evan.design.pattern.behavioral.visitor;
+
+
+public interface IVisitor {
+
+    void visit(FreeCourse freeCourse);
+
+    void visit(CodingCourse codingCourse);
+
+
+}
